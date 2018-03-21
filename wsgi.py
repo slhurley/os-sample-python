6e1bc84ba636ed2a54369e804a92440fd5394025
Sharolyn Hurley <sharolyn.hurley@gov.bc.ca>
2018-03-21 15:39:28 -0700

    testing

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! - My Name is Sharolyn"
+    return "Hello World! - My Name is Sharolyn Hurley"
 
 if __name__ == "__main__":
     application.run()
